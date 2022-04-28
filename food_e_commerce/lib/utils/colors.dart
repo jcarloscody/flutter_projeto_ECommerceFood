f19e38b8633da0ab85f93ec940b4f59bd3e434b9
Josue <josuecarlosos2@gmail.com>
2022-04-28 02:37:22 -0300

    widgets, cores, fontes

---
+++ food_e_commerce/lib/utils/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppCores {
+  //8:00
+  static final Color textoCor = Color.fromARGB(255, 185, 162, 92);
+  static final Color princCor = Color.fromARGB(255, 185, 162, 92);
+  static final Color iconeCor1 = Color.fromARGB(255, 185, 162, 92);
+  static final Color iconeCor2 = Color.fromARGB(255, 185, 162, 92);
+  static final Color paraCor = Color.fromARGB(255, 185, 162, 92);
+  static final Color botaoBackgroud = Color.fromARGB(255, 185, 162, 92);
+  static final Color sinalCor = Color.fromARGB(255, 185, 162, 92);
+  static final Color tituloCor = Color.fromARGB(255, 185, 162, 92);
+  static final Color prinPretoCor = Color.fromARGB(255, 185, 162, 92);
+  static final Color amareloCor = Color.fromARGB(255, 185, 162, 92);
+}
---
+++ food_e_commerce/lib/widgets/big_text.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class BigTextWidget extends StatelessWidget {
+  final Color? color;
+  final String text;
+  double size;
+  TextOverflow overflow;
+
+  BigTextWidget(
+      {Key? key,
+      this.color,
+      required this.text,
+      this.size = 20,
+      this.overflow = TextOverflow.ellipsis})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      overflow: overflow,
+      style: TextStyle(
+        fontSize: size,
+        color: color,
+        fontWeight: FontWeight.w400,
+        fontFamily: 'Roboto',
+      ),
+    );
+  }
+}
